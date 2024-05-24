e6488931ad582aeedec73e3616c5ec7456c898bf
Yash Jain <yash.jain@capanicus.com>
2024-05-24 18:06:16 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,28 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import customParser from "socket.io-msgpack-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const httpServer = createServer(app);
+app.use(bodyParser);
 
 const io = new Server(httpServer, {
   cors: { origin: "*" },
   parser: customParser,
 });
 
-io.on("connection", async (socket) => {
-  
-});
+io.on("connection", async (socket) => {});
 
 httpServer.listen(4000, "localhost", () => {
-  console.log("Server is listening to", 4000);
+  console.log("Socket Server is listening to", 4000);
+});
+
+app.post("/login", (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+});
+
+app.listen(6000, () => {
+  console.log(`API Server is running on port 6000`);
 });
