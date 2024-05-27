e142471912b971b7e8bb44b30c91ad59aa3f9000
Yash Jain <yash.jain@capanicus.com>
2024-05-27 16:29:27 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,29 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import customParser from "socket.io-msgpack-parser";
-import { expressMiddleware } from "@apollo/server/express4";
-import { ApolloServer } from "@apollo/server";
 import { bodyParserMiddleWare, corsMiddleWare } from "./middlewares.js";
+import cassandra from "cassandra-driver";
+
+const cloud = { secureConnectBundle: process.env["DATACENTER_ID"] };
+const authProvider = new cassandra.auth.PlainTextAuthProvider("token", process.env["APPLICATION_TOKEN"]);
+const client = new cassandra.Client({ cloud, authProvider });
+
+async function connectDatabase() {
+  await client.connect();
+}
 
 async function startApolloServer() {
   const app = express();
-  const apolloServer = new ApolloServer({});
+  // const apolloServer = new ApolloServer({});
   bodyParserMiddleWare(app);
   corsMiddleWare(app);
 
-  await apolloServer.start();
-  app.use("/graphql", expressMiddleware(apolloServer));
+  // await apolloServer.start();
+  // app.use("/graphql", expressMiddleware(apolloServer));
+  app.get("/", (req, res) => {
+    connectDatabase();
+    res.send("Hello");
+  });
   app.listen(4000, () => {
     console.log("Server is running on PORT 4000");
   });
