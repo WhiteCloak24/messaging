3d11c588b79ee51b2cc5fad9e83deff0ead358c0
Yash Jain <yash.jain@capanicus.com>
2024-05-28 15:22:55 +0530

    change

--- backend/resources/database.js
+++ backend/resources/database.js
@@ -3,7 +3,6 @@ import cassandra from "cassandra-driver";
 export let client;
 
 export async function connectDatabase() {
-  const authProvider = new cassandra.auth.PlainTextAuthProvider("token", process.env["APPLICATION_TOKEN"]);
   client = new cassandra.Client({
     cloud: {
       secureConnectBundle: "resources/secure-connect-messaging.zip",
