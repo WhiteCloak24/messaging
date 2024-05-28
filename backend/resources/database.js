03a4ae0f9aa667db71438183c569ec773abe7c63
Yash Jain <yash.jain@capanicus.com>
2024-05-28 14:54:25 +0530

    change

--- backend/resources/database.js
+++ backend/resources/database.js
@@ -3,12 +3,14 @@ import cassandra from "cassandra-driver";
 export let client;
 
 export async function connectDatabase() {
-  const authProvider = new cassandra.auth.PlainTextAuthProvider("token", process.env["APPLICATION_TOKEN"]);
   client = new cassandra.Client({
     cloud: {
       secureConnectBundle: "resources/secure-connect-messaging.zip",
     },
-    authProvider,
+    credentials: {
+      username: process.env["CLIENT_ID"],
+      password: process.env["CLIENT_SECRET"],
+      },
   });
   console.time("Connection with db created in");
   await client.connect();
