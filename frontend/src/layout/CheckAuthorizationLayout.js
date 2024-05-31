c771d9f2e4f37284948620cef02bf08e7e69d94d
Yash Jain <yash.jain@capanicus.com>
2024-05-31 13:57:18 +0530

    check

--- frontend/src/layout/CheckAuthorizationLayout.js
+++ frontend/src/layout/CheckAuthorizationLayout.js
@@ -7,8 +7,10 @@ const CheckAuthorizationLayout = () => {
   const { user_id, setUserId } = useAuthorization();
   useEffect(() => {
     window.addEventListener(AuthorizationEVENTS.SET_USER_ID, handleSetUser);
+    window.addEventListener(AuthorizationEVENTS.LOGGED_OUT, handleLogout);
     return () => {
       window.removeEventListener(AuthorizationEVENTS.SET_USER_ID, handleSetUser);
+      window.removeEventListener(AuthorizationEVENTS.LOGGED_OUT, handleLogout);
     };
   }, []);
 
@@ -17,6 +19,10 @@ const CheckAuthorizationLayout = () => {
       localStorage.user_id = e?.detail?.user_id;
       setUserId({ user_id: e?.detail?.user_id });
     }
+  }
+  function handleLogout(e) {
+      localStorage.clear()
+      // setUserId({ user_id: e?.detail?.user_id });
   }
   console.log({ user_id });
   return <Outlet />;
