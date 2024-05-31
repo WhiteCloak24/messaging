aa256f99afafbf32c2c0d0f9a8f37d294cab8eaf
Yash Jain <yash.jain@capanicus.com>
2024-05-31 15:53:12 +0530

    updates

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -6,7 +6,7 @@ export function bodyParserMiddleWare(app) {
   app.use(bodyParser.json());
 }
 export function corsMiddleWare(app) {
-  app.use(cors({ credentials: true, origin: ["http://localhost:3000", "http://192.168.29.209:3000"] }));
+  app.use(cors({ credentials: true, origin: ["http://localhost:8000", "http://192.168.29.209:8000"] }));
 }
 export function cookieParserMiddleWare(app) {
   app.use(cookieParser());
