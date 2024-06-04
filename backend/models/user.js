43ffb186fe5773bd26683191a076372dcf9de1eb
Yash Jain <yash.jain@capanicus.com>
2024-06-04 18:05:55 +0530

    changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,14 +6,11 @@ export const createUser = async ({ email = "", user_name = "", password = "" }) 
   const newUserId = uuidv4();
   const createdAt = getCurrentUTCTimestamp();
   try {
-    // const emailQuery = "INSERT INTO user_emails (email, user_id) VALUES (?, ?) IF NOT EXISTS";
-    // const emailResult = await client.execute(emailQuery, [email, newUserId], { prepare: true });
-    // if (emailResult.wasApplied()) {
-      const userQuery = "INSERT INTO users (user_id, created_at, user_name, email, password) VALUES (?, ?, ?, ?, ?)";
-      await client.execute(userQuery, [newUserId, createdAt, user_name, email, password], { prepare: true });
-    // } else {
-      // throw new Error("duplicate key error");
-    // }
+    const userQuery = "INSERT INTO users (user_id, created_at, user_name, email, password) VALUES (?, ?, ?, ?, ?) IF NOT EXISTS";
+    const emailResult = await client.execute(userQuery, [newUserId, createdAt, user_name, email, password], { prepare: true });
+    if (!emailResult.wasApplied()) {
+      throw new Error("duplicate key error");
+    }
   } catch (err) {
     if (err.message.includes("duplicate key error")) {
       throw new Error("Email already exists");
