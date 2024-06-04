ce9e0ff9a6b208777eb0e4f52db91e3091a09103
Yash Jain <yash.jain@capanicus.com>
2024-06-04 17:59:57 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,17 +4,27 @@ export let client;
 
 export async function connectDatabase() {
   client = new cassandra.Client({
-    cloud: {
-      secureConnectBundle: "services/secure-connect-messaging.zip",
-    },
-    credentials: {
-      username: process.env["CLIENT_ID"],
-      password: process.env["CLIENT_SECRET"],
-    },
-    keyspace: "admin",
+    contactPoints: ['127.0.0.1'],  // Replace with your Cassandra node IPs
+    localDataCenter: 'datacenter1' // Replace with your data center name
   });
   console.time("Connection with db created in");
   await client.connect();
   await client.execute("USE admin");
   console.timeEnd("Connection with db created in");
 }
+// export async function connectDatabase() {
+//   client = new cassandra.Client({
+//     cloud: {
+//       secureConnectBundle: "services/secure-connect-messaging.zip",
+//     },
+//     credentials: {
+//       username: process.env["CLIENT_ID"],
+//       password: process.env["CLIENT_SECRET"],
+//     },
+//     keyspace: "admin",
+//   });
+//   console.time("Connection with db created in");
+//   await client.connect();
+//   await client.execute("USE admin");
+//   console.timeEnd("Connection with db created in");
+// }
