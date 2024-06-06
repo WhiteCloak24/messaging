5288ec1d5835f3b69749bc29a9e6f6bc69ff6c17
Yash Jain <yash.jain@capanicus.com>
2024-06-06 13:56:06 +0530

    changes

--- frontend/src/layout/AuthorizedLayout.js
+++ frontend/src/layout/AuthorizedLayout.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useAuthorization } from "../hooks/useAuthorization";
 import { AuthorizationStates } from "../resources/constants";
 import { Navigate } from "react-router-dom";
+import { useApplicationSocket } from "../hooks/useApplicationSocket";
 
 const AuthorizedLayout = () => {
   const { authorizationState } = useAuthorization();
+  const { isSocketConnected, unsubscribeSocket } = useApplicationSocket();
+
+  useEffect(() => {
+    return () => {
+      if (isSocketConnected) {
+        unsubscribeSocket();
+      }
+    };
+  }, [isSocketConnected]);
 
   if (authorizationState === AuthorizationStates.LOGGED_IN) {
     return (
