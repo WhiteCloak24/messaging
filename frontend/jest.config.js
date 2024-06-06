b8467ad842dbe81e18afc4fb1b6cc157e7d5a9f6
Yash Jain <yash.jain@capanicus.com>
2024-06-06 16:13:13 +0530

    changes

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.js'],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  };
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+};
--- frontend/tests/signup.test.js
+++ frontend/tests/signup.test.js
@@ -1,6 +1,7 @@
-const axios = require('axios');
+const axios = require('axios')
 
 describe("Signup 1000 Users", () => {
+  const baseUrl = "http://localhost:4000/auth/signup";
 
   for (let i = 1; i <= 1000; i++) {
     const user_name = `user${i}`;
@@ -8,14 +9,11 @@ describe("Signup 1000 Users", () => {
     const password = "password123";
 
     test(`should sign up user ${i}`, async () => {
-      const response = await axios.post('http://localhost:4000/auth/signup', {
+      const response = await axios.post(baseUrl, {
         user_name,
         email,
-        password
+        password,
       });
-
-      console.log(response);
-
       expect(response.status).toBe(201); // Adjust based on your API response
     });
   }
