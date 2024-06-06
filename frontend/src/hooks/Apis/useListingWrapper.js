14a3391a648f7e9b552b2fe9c5232ebb3e556923
Yash Jain <yash.jain@capanicus.com>
2024-06-06 18:06:57 +0530

    fix

--- frontend/src/hooks/Apis/useListingWrapper.js
+++ frontend/src/hooks/Apis/useListingWrapper.js
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useListingWrapper = ({ queryKey = "", queryFn = () => null }) => {
-  const Request = useQuery({ queryKey, queryFn });
-  return { Request };
+  const Request = useQuery({ queryKey, queryFn, select: (data) => data?.data?.data || [] });
+  return { ...Request };
 };
 
 export default useListingWrapper;
