c73ea8b0c3cc20e568f63006c0d0cbaaf192024c
Yash Jain <yash.jain@capanicus.com>
2024-06-07 16:53:29 +0530

    changes

--- frontend/src/components/ImagePreviewer/index.js
+++ frontend/src/components/ImagePreviewer/index.js
@@ -1,9 +1,10 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { AvatarPreview } from "../../resources/constants";
 
 const ImagePreviewer = () => {
   const previewRef = useRef(null);
   const previewContainerRef = useRef(null);
+  const [isImagePreview, setIsImagePreview] = useState(false);
   useEffect(() => {
     window.addEventListener(AvatarPreview, handleAvatarPreview);
     return () => window.removeEventListener(AvatarPreview, handleAvatarPreview);
@@ -23,7 +24,9 @@ const ImagePreviewer = () => {
   }
   const openPreview = useCallback(() => {
     previewRef.current.classList.add("top-0");
+    setIsImagePreview(true);
   }, []);
+
   const closePreview = useCallback(() => {
     const container = previewContainerRef.current;
     previewRef.current.classList.remove("top-0");
@@ -34,9 +37,13 @@ const ImagePreviewer = () => {
     }
   }, []);
   return (
-    <div ref={previewRef} className="h-screen w-screen bg-black opacity-55 fixed flex flex-col items-center justify-center">
-      <div className="fixed top-2 right-5 bg-slate-50 p-3 rounded-md cursor-pointer" onClick={closePreview}>Close</div>
-      <div ref={previewContainerRef} id="preview-container" className="h-3/4 bg-black overflow-hidden"></div>
+    <div ref={previewRef} className="h-screen w-screen bg-black bg-opacity-55 fixed flex flex-col items-center justify-center">
+      {isImagePreview && (
+        <div className="fixed top-2 right-5 bg-slate-50 p-3 rounded-md cursor-pointer" onClick={closePreview}>
+          Close
+        </div>
+      )}
+      <div ref={previewContainerRef} id="preview-container" className="h-3/4 bg-black overflow-hidden opacity-100"></div>
     </div>
   );
 };
