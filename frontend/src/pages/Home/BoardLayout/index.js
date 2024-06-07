0a7be167203babbb8619c35378f8ec922ee2761c
Yash Jain <yash.jain@capanicus.com>
2024-06-07 13:35:58 +0530

    changes

--- frontend/src/pages/Home/BoardLayout/index.js
+++ frontend/src/pages/Home/BoardLayout/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BoardLayout = () => {
-  return <div className="border w-3/4"></div>;
+  return <div className="border w-4/5"></div>;
 };
 
 export default BoardLayout;
--- frontend/src/pages/Home/SidebarLayout/index.js
+++ frontend/src/pages/Home/SidebarLayout/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import SidebarHeader from "./SidebarHeader";
-import useListingWrapper from "../../../hooks/Apis/useListingWrapper";
-import { userListing } from "../../../api-service";
-import SidebarListing from "./SidebarListing";
+import UserListing from "./UserListing";
 
 const SidebarLayout = () => {
-  const { data } = useListingWrapper({ queryFn: userListing, queryKey: ["userListing"] });
-  console.log({ data });
   return (
-    <div className="border w-1/4">
+    <div className="border w-1/5 flex flex-col">
       <SidebarHeader />
-      <SidebarListing />
+      <UserListing />
     </div>
   );
 };
