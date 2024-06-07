7fe02849e0d9485fc94390a42dda5af7021301d6
Yash Jain <yash.jain@capanicus.com>
2024-06-07 15:47:51 +0530

    changes

--- frontend/src/pages/Home/ChatMessages/index.js
+++ frontend/src/pages/Home/ChatMessages/index.js
@@ -1,16 +1,17 @@
-
-import React from 'react';
+import React from "react";
 const messages = [
   { type: "sent", content: "I hope these article helps.", time: "May 10, 2022, 11:14 AM" },
+  { type: "sent", content: "I hope these article helps.", time: "May 10, 2022, 11:14 AM" },
   { type: "received", content: "https://www.envato.com/atomic-power-plant-engine/", time: "May 10, 2022, 11:14 AM" },
+
   // Add other messages here...
 ];
 const ChatMessages = () => {
   return (
-    <div className="flex-1 p-4 overflow-y-scroll">
+    <div className="flex-1 p-4 overflow-y-scroll flex flex-col">
       {messages.map((msg, index) => (
-        <div key={index} className={`mb-4 ${msg.type === 'sent' ? ' bg-white' : 'bg-customDarkblue text-white'}`}>
-          <p className="inline-block p-2 rounded">{msg.content}</p>
+        <div key={index} className={`mb-4 w-fit h-fit rounded-md ${msg.type === "sent" ? "self-end" : "self-start"}`}>
+          <p className={`inline-block p-2 rounded  ${msg.type === "sent" ? " bg-white" : "bg-customDarkblue text-white"}`}>{msg.content}</p>
           <p className="text-xs text-gray-500">{msg.time}</p>
         </div>
       ))}
