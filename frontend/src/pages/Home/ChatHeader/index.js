d5e9d87841f309a3e4af93c43ce5c88e5532b6ad
Yash <yashjain200024@gmail.com>
2024-06-10 19:02:35 +0530

    changes

--- frontend/src/pages/Home/ChatHeader/index.js
+++ frontend/src/pages/Home/ChatHeader/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import Avatar from "../../../components/Avatar";
 const ChatHeader = ({ name, status }) => {
   return (
     <div className="w-full flex items-center p-4 border-b bg-white">
-      <img src="https://via.placeholder.com/40" alt="profile" className="rounded-full mr-4"/>
-      <div>
+      <Avatar imgSrc="https://via.placeholder.com/40" />
+      {/* <img
+        src="https://via.placeholder.com/40"
+        alt="profile"
+        className="rounded-full mr-4"
+      /> */}
+      <div className="ml-4">
         <p className="font-bold">{name}</p>
         <p className="text-sm text-gray-600">{status}</p>
       </div>
--- frontend/src/pages/Home/ChatInput/index.js
+++ frontend/src/pages/Home/ChatInput/index.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { FaRegImage } from "react-icons/fa6";
 import { FaSmile, FaMicrophone, FaTelegramPlane } from "react-icons/fa";
 const ChatInput = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioURL, setAudioURL] = useState("");
+  const mediaRecorder = useRef(null);
+  const audioChunks = useRef([]);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder.current = new MediaRecorder(stream);
+    mediaRecorder.current.ondataavailable = (event) => {
+      console.log(event);
+      audioChunks.current.push(event.data);
+    };
+
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks.current, { type: "audio/wav" });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioURL(audioUrl);
+      audioChunks.current = [];
+    };
+
+    mediaRecorder.current.start();
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    mediaRecorder.current.stop();
+    setIsRecording(false);
+  };
   return (
     <div className="w-full flex p-4 border-t bg-white items-center gap-3">
       <div className="bg-gray-200 h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:text-amber-700">
@@ -14,13 +42,24 @@ const ChatInput = () => {
       <div className="bg-gray-200 h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:text-yellow-600">
         <FaSmile />
       </div>
-      <input type="text" placeholder="Type a message..." className="flex-1 p-2 rounded border" />
-      <div className="cursor-pointer">
-        <FaMicrophone />
-      </div>
+      <input
+        type="text"
+        placeholder="Type a message..."
+        className="flex-1 p-2 rounded border"
+      />
+      {!isRecording ? (
+        <div className="cursor-pointer" onClick={startRecording}>
+          <FaMicrophone />
+        </div>
+      ) : (
+        <div className="cursor-pointer" onClick={stopRecording}>
+          Stop
+        </div>
+      )}
       <div className="cursor-pointer">
         <FaTelegramPlane />
       </div>
+      {audioURL && <audio src={audioURL} controls />}
     </div>
   );
 };
