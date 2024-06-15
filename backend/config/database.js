f65856d3805f32b93d1a380dd880803101b62ced
Yash <yashjain200024@gmail.com>
2024-06-15 16:05:13 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,27 +4,13 @@ export let client;
 
 export async function connectDatabase() {
   client = new cassandra.Client({
-    contactPoints: ['127.0.0.1'],  // Replace with your Cassandra node IPs
-    localDataCenter: 'datacenter1' // Replace with your data center name
+    contactPoints: ["34.93.23.0"], // Replace with your Cassandra node IPs
+    localDataCenter: "datacenter1", // Replace with your data center name
   });
   console.time("Connection with db created in");
   await client.connect();
-  await client.execute("USE admin");
+  // await client.execute("USE admin");
   console.timeEnd("Connection with db created in");
 }
-// export async function connectDatabase() {
-//   client = new cassandra.Client({
-//     cloud: {
-//       secureConnectBundle: "services/secure-connect-messaging.zip",
-//     },
-//     credentials: {
-//       username: process.env["CLIENT_ID"],
-//       password: process.env["CLIENT_SECRET"],
-//     },
-//     keyspace: "admin",
-//   });
-//   console.time("Connection with db created in");
-//   await client.connect();
-//   await client.execute("USE admin");
-//   console.timeEnd("Connection with db created in");
-// }
+
+// https://cassandra.apache.org/doc/latest/cassandra/getting-started/
