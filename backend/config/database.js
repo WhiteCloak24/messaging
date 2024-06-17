b3b15b873b5b35b941348e3922664660af6d36ee
Yash Jain <yash.jain@capanicus.com>
2024-06-17 12:10:45 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,7 @@ export async function connectDatabase() {
   client = new cassandra.Client({
     contactPoints: ["34.93.23.0"], // Replace with your Cassandra node IPs
     localDataCenter: "datacenter1", // Replace with your data center name
+    keyspace: "development",
   });
   console.time("Connection with db created in");
   await client.connect();
