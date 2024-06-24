6f9a41a27354cf15e3194942eec23f7d26988274
Yash Jain <yash.jain@capanicus.com>
2024-06-24 14:02:29 +0530

    fix

--- frontend/src/pages/Home/HomeIconCanvas/index.js
+++ frontend/src/pages/Home/HomeIconCanvas/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef, useEffect } from "react";
 
 const HomeIconCanvas = ({ size = 400 }) => {
   const canvasRef = useRef(null);
