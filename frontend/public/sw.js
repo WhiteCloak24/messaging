7b2812faccfe6a53eb9e5e8b338612dd5258f4a5
Yash Jain <yash.jain@capanicus.com>
2024-06-25 18:55:00 +0530

    added offline mode in service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -2,18 +2,27 @@ const staticCacheName = "static-cache";
 const assets = ["/", "/index.html"];
 
 self.addEventListener("install", (evt) => {
-  caches
-    .open(staticCacheName)
-    .then((cache) => {
-      cache.addAll([]);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  evt.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then((cache) => {
+        cache.addAll(["/static/js/bundle.js", "/index.html", "/"]);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  );
 });
 self.addEventListener("activate", (evt) => {
   console.log("Service worker has been activated");
 });
 self.addEventListener("fetch", (evt) => {
   //   console.log("Fetch Event", evt);
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => {
+      if (resp) {
+        return resp;
+      }
+    })
+  );
 });
