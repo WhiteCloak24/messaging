cc5ae94481ede1d0e572d0fe6ec8010d4554c50a
Yash Jain <yash.jain@capanicus.com>
2024-06-26 11:29:39 +0530

    changes

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -41,12 +41,12 @@ const Login = () => {
   }
 
   return (
-    <div className="flex h-screen bg-gray-200 items-center justify-center px-4 sm:px-6 lg:px-8">
-      <div className="bg-white p-8 rounded shadow-md sm:w-full lg:w-1/2">
-        <h2 className="text-gray-800 text-2xl font-semibold mb-4 text-center">Login</h2>
-        <form className="w-full">
+    <div className="bg-gray-200 flex items-center justify-center min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <h2 className="text-gray-800 text-2xl font-semibold mb-6 text-center">Login</h2>
+        <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
               Email
             </label>
             <Controller
@@ -57,15 +57,16 @@ const Login = () => {
                   <input
                     {...field}
                     type="email"
+                    id="email"
                     placeholder="Enter your email"
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   />
                 );
               }}
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Password
             </label>
             <Controller
@@ -75,24 +76,24 @@ const Login = () => {
                 return (
                   <input
                     {...field}
-                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password"
                     type="password"
+                    id="password"
                     placeholder="Enter your password"
+                    className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   />
                 );
               }}
             />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
             <button
-              className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={handleSubmit(onSubmit)}
-              disabled={isPending}>
+              disabled={isPending}
+              className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 sm:mb-0 w-full sm:w-auto">
               {isPending ? "Please wait..." : "Sign In"}
             </button>
-            <button className="text-gray-600 hover:text-gray-700 text-sm" type="button" onClick={() => navigate("signup")}>
+            <button type="button" onClick={() => navigate("signup")} className="text-gray-600 hover:text-gray-700 text-sm w-full sm:w-auto text-center">
               Sign Up
             </button>
           </div>
