a53f0f6bd8771e76f0800b91e25b7c9d256224fa
Yash Jain <yash.jain@capanicus.com>
2024-06-26 13:00:13 +0530

    cha

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,27 +1,27 @@
-const staticCacheName = "static-cache";
-const assets = ["/", "/index.html"];
+// const staticCacheName = "static-cache";
+// const assets = ["/", "/index.html"];
 
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches
-      .open(staticCacheName)
-      .then((cache) => {
-        cache.addAll(["/static/js/bundle.js", "/manifest.json", "/favicon.ico", "/logo192.png", "/index.html", "/", "/signup"]);
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  );
-});
-self.addEventListener("activate", (evt) => {
-  console.log("Service worker has been activated");
-});
-self.addEventListener("fetch", (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((resp) => {
-      if (resp) {
-        return resp || fetch(evt.request);
-      }
-    })
-  );
-});
+// self.addEventListener("install", (evt) => {
+//   evt.waitUntil(
+//     caches
+//       .open(staticCacheName)
+//       .then((cache) => {
+//         cache.addAll(["/static/js/bundle.js", "/manifest.json", "/favicon.ico", "/logo192.png", "/index.html", "/", "/signup"]);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       })
+//   );
+// });
+// self.addEventListener("activate", (evt) => {
+//   console.log("Service worker has been activated");
+// });
+// self.addEventListener("fetch", (evt) => {
+//   evt.respondWith(
+//     caches.match(evt.request).then((resp) => {
+//       if (resp) {
+//         return resp || fetch(evt.request);
+//       }
+//     })
+//   );
+// });
