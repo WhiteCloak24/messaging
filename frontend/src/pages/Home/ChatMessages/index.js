0bf4bfbdeab8bf4c21ab432d264b6d701333695d
Yash Jain <yash.jain@capanicus.com>
2024-06-26 18:10:01 +0530

    changes

--- frontend/src/pages/Home/ChatMessages/index.js
+++ frontend/src/pages/Home/ChatMessages/index.js
@@ -5,8 +5,8 @@ import { IoCheckmarkOutline } from "react-icons/io5";
 import { IoCheckmarkDoneOutline } from "react-icons/io5";
 
 const MessageStatusIconMap = {
-  Sent: <IoCheckmarkOutline />,
-  Delivered: <IoCheckmarkDoneOutline />,
+  Sent: <IoCheckmarkOutline className="w-full h-full" />,
+  Delivered: <IoCheckmarkDoneOutline className="w-full h-full" />,
 };
 const ChatMessages = ({ activeChat = {} }) => {
   const { fetchMessageListing, messageListing, user_id, setActiveChat } = useApplicationSocket();
@@ -24,11 +24,11 @@ const ChatMessages = ({ activeChat = {} }) => {
           <div
             key={chat_id}
             className={`mb-4 w-fit h-fit px-4 py-2 flex gap-4 max-w-[60%] rounded-md ${
-              isSent ? "self-end bg-customDarkblue text-white" : "self-start bg-white"
+              isSent ? "self-end bg-blue-600 text-white" : "self-start bg-white"
             }`}>
             <div className="flex self-start whitespace-pre-wrap break-all">{msg?.message_text}</div>
             <div className={`text-xs ${isSent ? "text-gray-100" : "text-gray-500"} flex self-end min-w-16`}>
-              {formatTime({ timestamp: msg?.sent_time })}&nbsp; <span className="h-full">{MessageStatusIconMap["Delivered"]}</span>
+              {formatTime({ timestamp: msg?.sent_time })}&nbsp; <span className="w-4 h-4 text-green-200">{MessageStatusIconMap["Delivered"]}</span>
             </div>
           </div>
         );
