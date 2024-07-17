6726672bdaeaeaeaf5a70fb82033e23d2b12b618
Yash Jain <yash.jain@capanicus.com>
2024-07-17 18:46:32 +0530

    updated be cors

--- backend/middlewares/appMiddleware.js
+++ backend/middlewares/appMiddleware.js
@@ -6,7 +6,7 @@ export function bodyParserMiddleWare(app) {
   app.use(bodyParser.json());
 }
 export function corsMiddleWare(app) {
-  app.use(cors({ credentials: true, origin: ["http://localhost:8000", "http://192.168.29.209:8000"] }));
+  app.use(cors({ credentials: true, origin: ["http://localhost:8000", "http://192.168.29.209:8000","https://messaging.nightsoft.in"] }));
 }
 export function cookieParserMiddleWare(app) {
   app.use(cookieParser());
