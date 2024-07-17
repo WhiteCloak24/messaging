ad58e8c85792045fef543e81d275e2360193dd5e
Yash Jain <yash.jain@capanicus.com>
2024-07-17 14:06:09 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,8 @@ import cassandra from "cassandra-driver";
 export let client;
 
 export async function connectDatabase() {
-  console.log(process.env.CASSANDRA_DB_IP);
   client = new cassandra.Client({
-    contactPoints: ["34.93.0.213"], // Replace with your Cassandra node IPs
+    contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs
     // contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs
     localDataCenter: "datacenter1", // Replace with your data center name
     // keyspace: "admin",
