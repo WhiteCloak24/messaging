f1a4874747e9be017fb46a3eba5fab6ee7e61142
Yash Jain <yash.jain@capanicus.com>
2024-07-17 13:20:11 +0530

    db env updates

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ export let client;
 export async function connectDatabase() {
   client = new cassandra.Client({
     // contactPoints: ["34.93.23.0"], // Replace with your Cassandra node IPs
-    contactPoints: ["127.0.0.1"], // Replace with your Cassandra node IPs
+    contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs
     localDataCenter: "datacenter1", // Replace with your data center name
     keyspace: "admin",
   });
