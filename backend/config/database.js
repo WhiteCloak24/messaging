8425006f1e4343a150f1aa9f6b28e42dafb334d7
Yash Jain <yash.jain@capanicus.com>
2024-07-17 17:51:00 +0530

    fixed db keyspace

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ export let client;
 export async function connectDatabase() {
   client = new cassandra.Client({
     contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs
-    // contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs
+    // contactPoints: [process.env.CASSANDRA_DB_IP], // Replace with your Cassandra node IPs 
     localDataCenter: "datacenter1", // Replace with your data center name
     // keyspace: "admin",
   });
