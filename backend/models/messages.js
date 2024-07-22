5242ab9c3e021948580b362ecb67168e9bd3311f
Yash Jain <yash.jain@capanicus.com>
2024-07-22 16:44:48 +0530

    fix

--- backend/models/messages.js
+++ backend/models/messages.js
@@ -3,11 +3,12 @@ import { generateChatId } from "../utils/index.js";
 
 export const sendMessage = async ({ user_id = "", receiverId, message = "", sent_time, timeUUID, attachment = "" }) => {
   try {
-    const query = `INSERT INTO messages (chat_id, message_id, is_read, message_text, recipient_id, sender_id, sent_time, attachment) VALUES ( ?, ?, ?, ?, ?, ?, ? );`;
+    const query = `INSERT INTO messages (chat_id, message_id, is_read, message_text, recipient_id, sender_id, sent_time, attachment) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);`;
     const chat_id = generateChatId({ senderId: user_id, receiverId });
     await client.execute(query, [chat_id, timeUUID, false, message, receiverId, user_id, sent_time, attachment], { prepare: true });
     return true;
   } catch (e) {
+    console.log(e.message);
     return false;
   }
 };
