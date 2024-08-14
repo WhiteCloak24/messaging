a08d80f3e84439eb60c7e3142386b88a2252bbe5
Yash Jain <yash.jain@capanicus.com>
2024-08-14 10:53:39 +0530

    fix

--- frontend/src/components/MicAudioRecorder/index.js
+++ frontend/src/components/MicAudioRecorder/index.js
@@ -96,12 +96,13 @@ const MicAudioRecorder = ({ onClose = () => null }) => {
         return normalizedLevel;
       };
       function handleGetAudioLevel() {
-        let level = getAudioLevel();
-        if (level < audiolevel) {
-          audiolevel = Math.max(0, audiolevel - 0.01);
-        } else {
-          audiolevel = Math.min(1, level);
-        }
+        // let level = getAudioLevel();
+        // if (level < audiolevel) {
+        //   audiolevel = Math.max(0, audiolevel - 0.01);
+        // } else {
+        //   audiolevel = Math.min(1, level);
+        // }
+        let audiolevel = Math.random()
         const mic_level_icon_container = document.getElementById("mic-level-icon-container");
         const mic_level_icon = document.getElementById("mic-level-icon");
         const mic_icon = document.getElementById("mic-icon");
